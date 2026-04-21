64923902a81a31a5a74f38ba8baa68f543040ef5
Masaki Murooka <m-murooka@aist.go.jp>
2026-04-21 14:51:39 +0900

    Temporarily skip MCC.

--- extensions/local.cmake
+++ extensions/local.cmake
@@ -2,4 +2,4 @@ include(${CMAKE_CURRENT_LIST_DIR}/interfaces/mc_mujoco.cmake)
 
 include(${CMAKE_CURRENT_LIST_DIR}/retargeting/retargeting.cmake)
 
-include(${CMAKE_CURRENT_LIST_DIR}/controllers/MultiContactController.cmake)
+# include(${CMAKE_CURRENT_LIST_DIR}/controllers/MultiContactController.cmake)
