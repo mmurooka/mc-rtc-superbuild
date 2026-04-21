0db8f89ed330f71566022a3a5ea1b8711b3d5fbf
Masaki Murooka <m-murooka@aist.go.jp>
2026-04-21 12:57:15 +0900

    Add mc_mujoco

---
+++ extensions/interfaces/mc_mujoco.cmake
@@ -0,0 +1,50 @@
+include(${CMAKE_CURRENT_LIST_DIR}/../simulation/MuJoCo.cmake)
+
+AptInstall(libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libglew-dev)
+
+AddProject(mc_mujoco
+  GITHUB rohanpsingh/mc_mujoco
+  GIT_TAG origin/main
+  CMAKE_ARGS -DMUJOCO_ROOT_DIR=${MUJOCO_ROOT_DIR}
+  DEPENDS mc_rtc
+)
+
+if(WITH_HRP5)
+  AddProject(hrp5p_mj_description
+    GITHUB_PRIVATE isri-aist/hrp5p_mj_description
+    GIT_TAG origin/main
+    DEPENDS mc_mujoco
+  )
+endif()
+
+if(WITH_HRP4CR)
+  AddProject(hrp4cr_mj_description
+    GITHUB_PRIVATE isri-aist/hrp4cr_mj_description
+    GIT_TAG origin/main
+    DEPENDS mc_mujoco
+  )
+endif()
+
+if(WITH_HRP4)
+  AddProject(hrp4_mj_description
+    GITE mc-hrp4/hrp4_mj_description
+    GIT_TAG origin/master
+    DEPENDS mc_mujoco hrp4_description
+  )
+endif()
+
+if(WITH_RHPS1)
+  AddProject(rhps1_mj_description
+    GITHUB_PRIVATE isri-aist/rhps1_mj_description
+    GIT_TAG origin/master
+    DEPENDS mc_mujoco rhps1_description
+  )
+endif()
+
+if(WITH_HUMAN)
+  AddProject(human_mj_description
+    GITHUB Hugo-L3174/human_mj_description
+    GIT_TAG origin/main
+    DEPENDS mc_mujoco human_description
+  )
+endif()
---
+++ extensions/simulation/MuJoCo.cmake
@@ -0,0 +1,36 @@
+if(DEFINED MUJOCO_CMAKE_INCLUDED)
+  return()
+endif()
+set(MUJOCO_CMAKE_INCLUDED TRUE)
+
+set(MUJOCO_VERSION 3.3.6)
+
+if(UNIX AND NOT APPLE AND NOT EMSCRIPTEN)
+  set(MUJOCO_ROOT_DIR "${CMAKE_INSTALL_PREFIX}/share/mujoco/mujoco-${MUJOCO_VERSION}")
+  if(MUJOCO_ROOT_DIR AND EXISTS ${MUJOCO_ROOT_DIR})
+    message(STATUS "MuJoCo is already installed in ${MUJOCO_ROOT_DIR}")
+    return()
+  endif()
+
+  set(MUJOCO_URL "https://github.com/deepmind/mujoco/releases/download/${MUJOCO_VERSION}/mujoco-${MUJOCO_VERSION}-linux")
+  if(${CMAKE_SYSTEM_PROCESSOR} MATCHES "arm")
+    set(MUJOCO_URL "${MUJOCO_URL}-aarch64.tar.gz")
+  else()
+    set(MUJOCO_URL "${MUJOCO_URL}-x86_64.tar.gz")
+  endif()
+  DownloadFile("${MUJOCO_URL}" "${CMAKE_CURRENT_BINARY_DIR}/mujoco/mujoco.tar.gz" "")
+  file(ARCHIVE_EXTRACT
+        INPUT "${CMAKE_CURRENT_BINARY_DIR}/mujoco/mujoco.tar.gz"
+        DESTINATION "${CMAKE_CURRENT_BINARY_DIR}/mujoco"
+  )
+  add_custom_target(install_mujoco ALL
+    COMMAND ${SUDO_OPT_CMD} ${CMAKE_COMMAND} -E copy_directory
+            "${CMAKE_CURRENT_BINARY_DIR}/mujoco"
+            "${CMAKE_INSTALL_PREFIX}/share/mujoco"
+    COMMENT "Copying MuJoCo to ${MUJOCO_ROOT_DIR}"
+  )
+else()
+  if(NOT DEFINED MUJOCO_ROOT_DIR)
+    message(FATAL_ERROR "MuJoCo must be installed manually on your platform, you must then defined MUJOCO_ROOT_DIR")
+  endif()
+endif()
