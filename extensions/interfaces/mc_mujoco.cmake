736c42ae6cf4d4477cf3ce01a0512f40ee3bf484
Masaki Murooka <m-murooka@aist.go.jp>
2026-05-07 22:43:20 +0900

    Use ros2 branch for rhp7_description.

--- extensions/interfaces/mc_mujoco.cmake
+++ extensions/interfaces/mc_mujoco.cmake
@@ -3,8 +3,8 @@ include(${CMAKE_CURRENT_LIST_DIR}/../simulation/MuJoCo.cmake)
 AptInstall(libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libglew-dev)
 
 AddProject(mc_mujoco
-  GITHUB rohanpsingh/mc_mujoco
-  GIT_TAG origin/main
+  GITHUB mmurooka/mc_mujoco
+  GIT_TAG origin/motion-reach
   CMAKE_ARGS -DMUJOCO_ROOT_DIR=${MUJOCO_ROOT_DIR}
   DEPENDS mc_rtc
 )
--- robots/RHP7.cmake
+++ robots/RHP7.cmake
@@ -7,15 +7,15 @@ endif()
 AddCatkinProject(
   rhp7_description
   GITHUB_PRIVATE isri-aist/rhp7_description
-  GIT_TAG origin/motion-reach
+  GIT_TAG origin/motion-reach-ros2
   WORKSPACE data_ws
   CMAKE_ARGS ${MC_RTC_ROS_OPTION}
 )
 
-# AddProject(
-#   mc_rhp7
-#   GITHUB_PRIVATE Yoshida-Lab-TUS/mc_rhp7
-#   GIT_TAG origin/motion-reach
-#   DEPENDS rhp7_description mc_rtc
-#   CMAKE_ARGS -DCMAKE_POLICY_VERSION_MINIMUM=3.5
-# )
+AddProject(
+  mc_rhp7
+  GITHUB_PRIVATE Yoshida-Lab-TUS/mc_rhp7
+  GIT_TAG origin/motion-reach
+  DEPENDS rhp7_description mc_rtc
+  CMAKE_ARGS -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+)
